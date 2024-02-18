30207dcb38bf4d8a82df11bddc90d4a64567545a
Avah Whitehead <avah.r.whitehead@gmail.com>
2024-02-18 13:16:29 +0000

    refactor: Extracted header into a dedicated component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderModule } from "./components/header/header.module";
 
 @NgModule({
 	declarations: [
@@ -12,7 +13,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
-		NgbModule
+		NgbModule,
+		HeaderModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
